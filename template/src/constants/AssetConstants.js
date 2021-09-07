06ba7166b62ed86025af6da43763ecc85a573aea
Syeda Maidah Rizvi <smaidah@folio3.com>
2021-09-07 15:26:55 +0500

    Add files via upload

---
+++ template/src/constants/AssetConstants.js
@@ -0,0 +1,7 @@
+export default {
+  logo: require('../../assets/images/logo.png'),
+  logout: require('../../assets/images/logout.png'),
+  menu: require('../../assets/images/menu.png'),
+  offline: require('../../assets/images/offline.png'),
+  offlineRed: require('../../assets/images/offline-red.png'),
+};
